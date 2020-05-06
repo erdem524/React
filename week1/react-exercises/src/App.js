9c88f81d42cef1dcb1c7d4a052dedb066ebaf7ba
enwer <tewbejan@gmail.com>
2020-05-06 20:40:49 +0200

    some improvement on week2

--- week1/react-exercises/src/App.js
+++ week1/react-exercises/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
-import Main from './chart/Main';
+import DogGallery from './week2/dogPhoto/DogGalary';
+import Friend from './week2/newFriend/Friend';
+import RandomJoke from './week2/joker/RandomJoke';
+import WeatherApp from './week2/project/WeatherApp';
 
 function App() {
 	return (
 		<div className="App">
-			<Main />
+			<Friend />
+			<DogGallery />
+			<RandomJoke />
+			<WeatherApp />
 		</div>
 	);
 }
